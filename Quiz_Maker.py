e3874aeca1de4827fe9300d3acdedad9beb9ad64
KingdomThought <79799753+KingdomThought@users.noreply.github.com>
2022-07-13 20:23:20 -0400

    Update Quiz_Maker.py
    Added logic to create_Quiz() function, in the menu.

--- Quiz_Maker.py
+++ Quiz_Maker.py
@@ -4,8 +4,17 @@ from numpy import record
 def create_Quiz():
     quizAuthor = input("What is your email adress\n")
     verify_Quiz_Author(quizAuthor)
-    school_Level = input("What Grade level is this quiz\n""1.Elementary School\n"
-                        "2.Junior HIgh School\n""3.High School\n""4.College\n")
+    school_Level = int(input("What Grade level is this quiz\n""1.Elementary School\n"
+                        "2.Junior HIgh School\n""3.High School\n""4.College\n"))
+    if school_Level == 1:
+        s_Level = "ES"
+    elif school_Level == 2:
+        s_Level = "JH"
+    elif school_Level == 3:
+        s_Level = "HS"
+    elif school_Level == 4:
+        s_Level = "UG"
+    print(s_Level)
 
     grade_Level = input("What Grade level is this quiz\n""1.Freshman\n"
                         "2.Sophmore\n""3.Junior\n""4.Senior\n") 
