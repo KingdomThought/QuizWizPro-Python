79b932a33a4e493a3d281796d4a725e8dd16eb93
KingdomThought <79799753+KingdomThought@users.noreply.github.com>
2022-07-14 17:21:53 -0400

    deleted unused files
    Deleted unused files

--- DB.py
+++
@@ -1,8 +0,0 @@
-import sqlite3 
- 
-con = sqlite3.connect('QWP.db')
-cur = con.cursor()
-cur.execute()
-con.commit
-cur.close
-con.close
--- import mysql connector.py
+++
@@ -1,4 +0,0 @@
-import mysql.connector
-mydb = mysql.connector.connect(host='127.0.0.1',  user ='megga', passwd ='meggaman')
-mycursor=mydb.cursor()
-mycursor.execute("show databses")
